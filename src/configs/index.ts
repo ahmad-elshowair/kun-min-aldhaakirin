9de6f4e4ff0f8e078a4ef3805091a57b304c01e7
Ahmad Elshowair <ahmad-elshowair.dev@outlook.com>
2024-12-30 07:30:22 +0700

    chore(config): remove unused supabaseServiceRoleKey from configuration

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,6 @@
 const configs = {
 	supabaseAnonKey: String(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY),
 	supabaseUrl: String(process.env.NEXT_PUBLIC_SUPABASE_URL),
-	supabaseServiceRoleKey: String(
-		process.env.SUPABASE_SERVICE_ROLE_KEY,
-	),
+	
 };
 export default configs;
